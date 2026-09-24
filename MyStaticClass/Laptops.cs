efc9fa46488ab5245578cf78762b697a989cdfac
vladradionov877-cloud <vlad.radionov877@gmail.com>
2026-09-24 22:38:04 +0300

    Add project files.

---
+++ MyStaticClass/Laptops.cs
@@ -0,0 +1,106 @@
+﻿namespace MyStaticClass
+{
+    class Laptops
+    {
+        private static int _countItems = 0;
+        private static int _maxScreenFrequency = 500;
+        private static int _maxMatrixLight = 1000;
+        private static int _maxSSDSize = 8;
+        public static int GetCountItems() { return _countItems; }
+        
+        private string _model;
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        private int _screenFrequency;
+        public int ScreenFrequency
+        {
+            get { return _screenFrequency; }
+            set
+            {
+                if (value >= _maxScreenFrequency)
+                {
+                    Console.WriteLine($"Частота екрану не може бути більше {_maxScreenFrequency}Гц!");
+                    return;
+                }
+                _screenFrequency = value;
+            }
+        }
+
+        private int _matrixLight;
+        public int MatrixLight
+        {
+            get { return _matrixLight; }
+            set
+            {
+                if (value >= _maxMatrixLight)
+                {
+                    Console.WriteLine($"Світло матриці не може бути більше {_maxMatrixLight}Нт!");
+                    return;
+                }
+                _matrixLight = value;
+            }
+        }
+
+        private int _ssdSize;
+        public int SSDSize
+        {
+            get { return _ssdSize; }
+            set
+            {
+                if (value >= _maxSSDSize)
+                {
+                    Console.WriteLine($"Розмір SSD не може бути більше {_maxSSDSize}Тб!");
+                    return;
+                }
+                _ssdSize = value;
+            }
+        }
+
+        // Конструктор за замовчуванням
+        public Laptops()
+        {
+            this.Model = "-";
+            this.ScreenFrequency = 0;
+            this.MatrixLight = 0;
+            this.SSDSize = 0;
+            _countItems++;
+        }
+
+        // Конструктор з параметрами
+        public Laptops(string model, int screenFrequency,
+            int matrixLight, int ssdSize)
+        {
+            this.Model = model;
+            this.ScreenFrequency = screenFrequency;
+            this.MatrixLight = matrixLight;
+            this.SSDSize = ssdSize;
+            _countItems++;
+        }
+
+        // Статичний конструктор
+        static Laptops()
+        {
+            _countItems = 0;
+            _maxScreenFrequency = 500;
+            _maxMatrixLight = 1000;
+            _maxSSDSize = 8;
+        }
+
+
+        public override string ToString()
+        {
+            return
+                $"\nМодель: {Model}" +
+                $"\nЧастота: {ScreenFrequency}Гц" +
+                $"\nСвітло матриці: {MatrixLight}Нт" +
+                $"\nSSD: {SSDSize}Тб";
+        }
+
+    }
+}
+
+
---
+++ MyStaticClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyStaticClass;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+/*
+Змінні спеціально мають бути вище тексту, де виводиться
+їх загальна к-ть через GetCountItems(). Якщо вони будуть
+під текстом, то виведеться значення '0' (за замовчуванням)
+*/
+
+// У консоль виведеться інфа з конструктора за замовчуванням
+Laptops laptop1 = new Laptops();
+
+// У консоль виведеться інфа з конструктора з параметрами
+Laptops laptop2 = new Laptops("Lenovo LOQ 15AHP10", 144, 300, 1);
+Laptops laptop3 = new Laptops("ASUS TUF Gaming F16", 144, 250, 1);
+Laptops laptop4 = new Laptops("Acer Extensa 15", 60, 250, 1);
+
+// У консоль виведеться інша зі статичного конструктора
+Laptops laptop5 = new Laptops();
+laptop5.Model = "MSI Cyborg 15";
+laptop5.ScreenFrequency = 144;
+laptop5.MatrixLight = 250;
+laptop5.SSDSize = 1;
+
+// Сам текст, основна мета якого вказати кількість наших об'єктів
+Console.WriteLine($"--- ТОП {Laptops.GetCountItems()} хороших ноутбуків ---");
+
+// А тепер це все добро можна вивести. Можна таким способ, а можна окремо
+Console.WriteLine($"{laptop1}\n{laptop2}\n{laptop3}\n{laptop4}\n{laptop5}");
